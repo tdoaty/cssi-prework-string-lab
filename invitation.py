3da4fcf73dcb02326e9116e9c372153bd0c07edc
tdoaty <tdoaty@google.com>
2017-04-27 17:42:39 -0400

    updated invitation.py

--- invitation.py
+++ invitation.py
@@ -10,28 +10,31 @@ def weasley_invitation(name,day,date,month):
 
 ### Challenge 3 - Seating Location
 def seating_location(last_name):
-    if last_name[0]== "A"
+    if last_name[0] == 'W'
+        location = "front row."
+    else:
+    if last_name[0] == "A"
         location = "rear section."
     else:
-    if last_name[0]== "B"
+    if last_name[0] == "B"
         location = "rear section."
     else:
-    if last_name[0]== "C"
+    if last_name[0] == "C"
         location = "rear section."
     else:
-    if last_name[0]== "D"
+    if last_name[0] == "D"
         location = "rear section."
     else:
-    if last_name[0]== "E"
+    if last_name[0] == "E"
             location = "rear section."
     else:
-    if last_name[0]== "F"
+    if last_name[0] == "F"
             location = "rear section."
     else:
-    if last_name[0]== "G"
+    if last_name[0] == "G"
             location = "rear section."
     else:
-    if  last_name[0]=="W"
+    if  last_name[0] =="W"
         location = "front row."
     else:
         location = "middle section."
@@ -39,5 +42,4 @@ def seating_location(last_name):
 
 ###Challenge 4 - All Together
 def create_invitation(first_name, last_name, message):
-  return "The %"
-  return "Replace this with your code"
+  return first_name.upper() + "./n" + message + "\nP.S. "+ seating_location(last_name)
