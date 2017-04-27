5e3bd412cff504a6d233e5b0fc148bfaacb9b619
tdoaty <tdoaty@google.com>
2017-04-27 12:49:06 -0400

    still working on seating and invtation

--- invitation.py
+++ invitation.py
@@ -1,11 +1,11 @@
 
 ### Challenge 1 - Percy Replacement:
 def percy_replacer(string_about_percy):
-  return "Replace this with your code"
+  return string_about_percy.replace("Percy", "Ron")
 
 ### Challenge 2 - String Interpolation:
 def weasley_invitation(name,day,date,month):
-  return "Replace this with your code"
+  return weasley_invitation.format("Ron", "Sunday", "May", "18th")
 ### Challenge 3 - Seating Location
 def seating_location(last_name):
   return "Replace this with your code"
